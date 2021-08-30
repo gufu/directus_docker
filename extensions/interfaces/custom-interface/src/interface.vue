5f277befebb8c30cd6a9b69c10636d0e4ef10e82
Konrad Gusz <k.gusz@cinema-city.pl>
2021-08-30 14:27:22 +0200

    [initial commit]

---
+++ extensions/interfaces/custom-interface/src/interface.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Test222</h1>
+  <input :value="value" @input="handleChange($event.target.value)" />
+</template>
+
+<script>
+export default {
+  emits: ['input'],
+  props: {
+    value: String,
+  },
+  methods: {
+    handleChange(value) {
+      this.$emit('input', value);
+    },
+  },
+};
+</script>
